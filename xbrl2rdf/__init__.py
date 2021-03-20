0849da7a716a539ee62662fd4a248aeee7ea8b8d
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2021-03-20 08:07:39 +0100

    __version__ added

--- xbrl2rdf/__init__.py
+++ xbrl2rdf/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for xbrl2rdf."""
 
+__author__ = """Willem Jan Willemse"""
+__email__ = 'w.j.willemse@xs4all.nl'
+__version__ = '0.1.0'
+
 from .utilfunctions import *
 from .const import *
 from .DtsProcessor import *
